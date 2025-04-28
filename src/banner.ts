b38b1abc8e6014da229bb48ac2b04b0cedc9f65e
matoruru <matoruru@shgt.jp>
2025-04-28 16:56:22 +0900

    fix: banner

--- src/banner.ts
+++ src/banner.ts
@@ -1,14 +1,12 @@
+export const banner = `
+ _                  _ _  __  __ 
+| | ___ __ ___   __| (_)/ _|/ _|
+| |/ / '_ \` _ \\ / _\` | | |_| |_ 
+|   <| | | | | | (_| | |  _|  _|
+|_|\\_\\_| |_| |_|\\__,_|_|_| |_|  
 
-export const banner = [
-  " _                  _ _  __  __ ",
-  "| | ___ __ ___   __| (_)/ _|/ _|",
-  "| |/ / '_ ` _ \\ / _` | | |_| |_ ",
-  "|   <| | | | | | (_| | |  _|  _|",
-  "|_|\\_\\_| |_| |_|\\__,_|_|_| |_|",
-  "",
-  "kmdiff - Kubernetes Manifest Diff",
-  "",
-].join("\n");
+kmdiff - Kubernetes Manifest Diff
+`.replace(/^\n/, '');
 
 export const printBanner = (): void => {
   console.log(banner);
